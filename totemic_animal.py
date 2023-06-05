ad7394e9be05124052067fdfce3d0ecf931589f8
vibe-34 <vibe.34@gmail.com>
2023-06-05 10:08:05 +0300

    add .env /.env_example /.gitignore /requirements.txt / добавлена кнопка VK с пометкой TODO \ deleted config.py and folder anibot_venv

--- totemic_animal.py
+++ totemic_animal.py
@@ -1,8 +1,12 @@
 import telebot
 from telebot import types
-import config
 
-bot = telebot.TeleBot(config.TOKEN)
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+bot = telebot.TeleBot(os.environ.get('TOKEN'))
 
 
 @bot.message_handler(commands=['start'])
@@ -10,7 +14,8 @@ def start(message):
     murkup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton('Начать викторину')
     btn2 = types.KeyboardButton('❓Задать вопрос❓')
-    murkup.add(btn1, btn2)
+    btn3 = types.KeyboardButton('Поделиться в VK')
+    murkup.add(btn1, btn2, btn3)
     bot.send_message(message.chat.id,
                      text="Привет, {0.first_name}! Кнопки под окном чата помогут сориентироваться".format(
                          message.from_user),
@@ -21,6 +26,11 @@ def start(message):
 def func(message):
     if message.text == 'Начать викторину':
         bot.send_message(message.chat.id, text='Викторина ещё в разработке')
+        # в elif добавил кнопку на VK (затем перенесем в нужное место) TODO
+    elif message.text == 'Поделиться в VK':
+        # text = '[Поделиться](https://vk.com/share.php?https://t.me/my_reincarnation_bot)'  # только шаблон
+        text = '[Поделиться](https://vk.com/share.php?url=http%3A%2F%2Ft.me/my_reincarnation_bot%2Ftest-16576%3Fresult%3D02040206020504080203&title=Мой%20результат%20теста%3A%20КТО%20ТВОЕ%20ТОТЕМНОЕ%20ЖИВОТНОЕ%3F%20)'
+        bot.send_message(message.chat.id, text, parse_mode='MarkdownV2')
     else:
         bot.send_message(message.chat.id, text='В данный момент всё в разработке')
 
