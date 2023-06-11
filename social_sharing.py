a493026e301eb290e93e0c5e05d76dd82a0838b8
vibe-34 <vibe.34@gmail.com>
2023-06-11 12:18:33 +0300

    add social_sharing.py

---
+++ social_sharing.py
@@ -0,0 +1,7 @@
+# ссылка на публикацию в ВК
+VK = '[Поделиться](https://vk.com/share.php?url=http%3A%2F%2Ft.me/my_reincarnation_bot%2F&title=Мой%20результат%20' \
+     'теста%3A%20КТО%20ТВОЕ%20ТОТЕМНОЕ%20ЖИВОТНОЕ%3F)'
+
+# ссылка на публикацию в Одноклассниках
+OK = '[Поделиться](https://connect.ok.ru/dk?cmd=WidgetSharePreview&st.cmd=WidgetSharePreview&st.shareUrl=' \
+     'http%3A%2F%2Ft.me/my_reincarnation_bot%2F&title=Мой%20результат%20теста%3A%20КТО%20ТВОЕ%20ТОТЕМНОЕ%20ЖИВОТНОЕ%3F)'
